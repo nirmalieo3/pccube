9fc6fa8d6f6d0c8cc8ea660a5b5e5b48c45399f1
C.F.Nirmalie Fernando Warnakulasuriya <wnirmalie@yahoo.com>
2021-11-03 09:38:59 +0100

    added a project

---
+++ PcGlovo/PcGlovoEAR/target/liberty/wlp/lib/features/checksums/com.ibm.websphere.appserver.injection-1.0.cs
@@ -0,0 +1,3 @@
+#Wed Nov 03 08:47:29 CET 2021
+lib/features/com.ibm.websphere.appserver.injection-1.0.mf=fb5fd39105662d1af9a42fe25f227687
+lib/com.ibm.ws.injection_1.0.58.jar=ee145a17c297db57c4ff56d5684047ba
---
+++ PcGlovo/PcGlovoEAR/target/liberty/wlp/lib/features/checksums/io.openliberty.servlet.api-3.1.cs
@@ -0,0 +1,3 @@
+#Wed Nov 03 08:47:28 CET 2021
+lib/features/io.openliberty.servlet.api-3.1.mf=27fc8eebc020c32206534bd75bb9c870
+dev/api/spec/com.ibm.websphere.javaee.servlet.3.1_1.0.58.jar=f5a713bfff71658a6b0f78932f182f1f
